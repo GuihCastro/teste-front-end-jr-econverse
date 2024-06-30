ad3a8724a522a33afd492d4482b2c01412a92f49
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-06-30 11:35:53 -0300

    Finalizei o modal

--- teste-front-end-jr-econverse/src/components/ProductCard/ProductCard.tsx
+++ teste-front-end-jr-econverse/src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,8 @@
 import styles from './ProductCard.module.scss';
 
+import { useState } from 'react';
 import Button from '../Button/Button';
+import ProductModal from '../ProductModal/ProductModal';
 interface ProductCardProps {
     name: string,
     short: string,
@@ -9,43 +11,46 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ name, short, photo, price }) => {
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
     return (
-        <li className={styles.container}>
-            <img src={photo} alt="Imagem do produto." />
-            <div className={styles.info}>
-                <p>{short}</p>
-                <div className={styles.price}>
-                    <h3>R${price}</h3>
-                    <h2>Ou 2x de R${price / 2} sem juros</h2>
+        <>
+            <li className={styles.container}>
+                <div className={styles.content} onClick={handleOpenModal}>
+                    <div className={styles.productMain}>
+                        <img src={photo} alt="Imagem do produto." />
+                        <p>{short}</p>
+                    </div>
+                    <div className={styles.info}>
+                        <div className={styles.price}>
+                            <h3>R$ {price}</h3>
+                            <h2>Ou 2x de R$ {(price / 2).toFixed(2)} sem juros</h2>
+                        </div>
+                        <span>Frete grátis</span>
+                    </div>
                 </div>
-                <span>Frete grátis</span>
-            </div>
-            <Button label='Comprar' />
-            {/* <h3>{name}</h3> */}
-        </li>
+                <Button label='Comprar' />
+                {/* <h3>{name}</h3> */}
+            </li>
+
+            <ProductModal 
+                open={openModal} 
+                close={handleCloseModal} 
+                name={name} 
+                short={short}
+                photo={photo}
+                price={price}
+            />
+        </>
     );
 }
 
 export default ProductCard;
-
-
-
-// import React from 'react';
-// import './ProductCard.module.scss';
-
-// interface ProductCardProps {
-//     product: any;
-//     onClick: () => void;
-// }
-
-// const ProductCard: React.FC<ProductCardProps> = ({ product, onClick }) => {
-//     return (
-//         <div className="product-card" onClick={onClick}>
-//             <img src={product.image} alt={product.name} />
-//             <h3>{product.name}</h3>
-//             <p>{product.price}</p>
-//         </div>
-//     );
-// };
-
-// export default ProductCard;
--- teste-front-end-jr-econverse/src/components/ProductModal/ProductModal.tsx
+++ teste-front-end-jr-econverse/src/components/ProductModal/ProductModal.tsx
@@ -1,3 +1,84 @@
+import styles from './ProductModal.module.scss';
+
+import { useState, useEffect } from 'react';
+import Button from '../Button/Button';
+
+import plus from '../../../public/assets/img/plus-icon.png';
+import minus from '../../../public/assets/img/minus-icon.png';
+
+interface ProductModalProps {
+    open: boolean,
+    close: () => void,
+    name: string,
+    short: string,
+    photo: string,
+    price: number
+}
+
+const ProductModal: React.FC<ProductModalProps> = ({ open, close, name, short, photo, price }) => {
+    const [quantity, setQuantity] = useState(1);
+
+    const handleIncrement = () => {
+        setQuantity(quantity => quantity + 1);
+    };
+
+    const handleDecrement = () => {
+        setQuantity(quantity => Math.max(1, quantity - 1));
+    };
+
+    useEffect(() => {
+        if (open) {
+            document.body.classList.add('overflow-hidden');
+        } else {
+            document.body.classList.remove('overflow-hidden');
+        }
+
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+        };
+    }, [open]);
+
+    if (!open) return null;
+
+    const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget) {
+            close();
+        }
+    };
+
+    return (
+        <div className={styles.container} onClick={handleBackdropClick}>
+            <div className={styles.modalCard}>
+                <button className={styles.closeBtn} onClick={close}></button>
+                <div className={styles.photo}>
+                    <img src={photo} alt="Imagem do produto." />
+                </div>
+                <div className={styles.info}>
+                    <h1>{name}</h1>
+                    <h2>R$ {price}</h2>
+                    <p>{short}</p>
+                    <a href="/">Veja mais detalhes do produto</a>
+                    <div className={styles.counter}>
+                        <button onClick={handleDecrement}>
+                            <img src={minus} alt="Ícone de decremento para diminuir a quantidade do produto." />
+                        </button>
+                        <span>{quantity}</span>
+                        <button onClick={handleIncrement}>
+                            <img src={plus} alt="Ícone de incremento para aumentar a quantidade do produto." />
+
+                        </button>
+                    </div>
+                    <Button label='Comprar' />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductModal;
+
+
+
 // import React from 'react';
 // import './ProductModal.module.scss';
 
