8c01792dddd2a15e1c5fb34491fe7bd25140e427
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-07-01 15:43:34 -0300

    nova atualização no vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        publicPath: '/public/',
+      },
+    },
   },
 })
