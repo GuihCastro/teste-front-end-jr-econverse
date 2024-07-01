43c44d6c4581b6097eb51c313b42675c4e7f84ed
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-07-01 15:38:29 -0300

    atualizei o vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://app.econverse.com.br',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
   },
 })
